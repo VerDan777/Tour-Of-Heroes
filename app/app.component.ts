4769b243550fc24962dd3bb9be4f304eca8bcddf
VerDan777 <vereshhagin98@mail.ru>
2017-04-22 16:05:16 +0300

    add forms

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,33 @@
-import {Component} from 'angular2/core';
+    import {Component} from 'angular2/core';
+    
+    export class Hero {
+        id:number;
+        name:string;
+    }
+    @Component({
+    selector:'my-app',
+    template: `<h1>{{title}}</h1>
+    <h2>{{hero.name}}</h2>
+    <div>
+        <label> id:</label>
+        {{hero.id}}
+        <label> name:</label>
+        {{hero.name}}
 
-@Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
-})
-export class AppComponent { }
+    </div>
+        <div> 
+        <label>name:</label>
+        <input placeholder="Name" [(ngModel)]="hero.name"/>
+        
+        </div>   `
+    })
+    
+    
+    export class AppComponent {
+    title="Tour of Heroes";
+    hero:Hero {
+    id:1,
+    name:"Winstorm"
+    };
+    }
+    
